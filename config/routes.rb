ac4e820d07abbe17098496204db674cd62d25a1f
NORI YUKI <norihito.yuki@me.com>
2015-08-17 01:52:06 +0900

    before editing rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+
+  root :to => 'static_pages#home'
+
+  get '/home',    to: 'static_pages#home'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,61 +2,67 @@ require 'rails_helper'
 
 RSpec.describe "Static Pages", type: :request do
 
+  subject {page}
+
   describe "Home page" do
+    before {visit root_path}
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_selector('h1', :text => 'Sample App')
+      expect(page).to have_selector('h1', text: 'Sample App')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
       expect(page).to have_selector('title',
-                      :text => "Ruby on Rails Tutorial Sample App", :visible => false)
+                      text: "Ruby on Rails Tutorial Sample App", :visible => false)
     end
 
     it "should not have custom page title" do
       visit '/static_pages/home'
       expect(page).not_to have_selector('title',
-                      :text => "| Home", :visible => false)
+                      text: "| Home", :visible => false)
     end
 
   end
 
   describe "Help page" do
+    before {visit help_path}
     it "should have the content 'Help page'" do
-      visit '/static_pages/help'
-      expect(page).to have_selector('h1', :text => 'Help page')
+      visit help_path
+      expect(page).to have_selector('h1', text: 'Help page')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_selector('title',
-                      :text => "Ruby on Rails Tutorial Sample App | Help", :visible => false)
+                      text: "Ruby on Rails Tutorial Sample App | Help", :visible => false)
 
     end
   end
 
   describe "About page" do
+    before {visit about_path}
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_selector('h1', :text => 'About Us')
+      visit about_path
+      expect(page).to have_selector('h1', text: 'About Us')
     end
     it "should have the right title" do
        visit '/static_pages/about'
       expect(page).to have_selector('title',
-                      :text => "Ruby on Rails Tutorial Sample App | About Us", :visible => false)
+                      text: "Ruby on Rails Tutorial Sample App | About Us", :visible => false)
 
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Content'" do
-      visit '/static_pages/contact'
-      expect(page).to have_selector('h1', :text => 'Contact')
+      visit contact_path
+      expect(page).to have_selector('h1', text: 'Contact')
     end
     it "should have the right title" do
-       visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_selector('title',
-                      :text => "Ruby on Rails Tutorial Sample App | Contact", :visible => false)
+                      text: "Ruby on Rails Tutorial Sample App | Contact", :visible => false)
 
     end
   end
