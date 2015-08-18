532bcdc34c1c987bf554580baef181e778b2701b
NORI YUKI <norihito.yuki@me.com>
2015-08-18 18:19:19 +0900

    users

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+  before_save { |user| user.email = user.email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sentitive: false}
+end
